7c4c31d9339ac99fbedf85a8e8b7b2ddbcd55e0f
PedroPK <pedropk@gmail.com>
2022-06-02 16:38:10 -0300

    Created new Module and Service to do Authentication

---
+++ catBooks/src/app/authentication/authentication.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+    constructor(
+        private httpClient:   HttpClient
+    ) { }
+
+    authenticate(pUser: string, pPassword: string): Observable<any> {
+        return this.httpClient
+            .post(
+                'http://localhost:3000/user/login',
+                {
+                    userName:   pUser,
+                    password:   pPassword
+                }
+          );
+    }
+
+}
