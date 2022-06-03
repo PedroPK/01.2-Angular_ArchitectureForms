eaebb5036570bf68b92c5d3cd7cbae3057bc866c
PedroPK <pedropk@gmail.com>
2022-06-03 14:34:17 -0300

    New Module and Component to show Messages
    Message are shown if Input fields are without content.
     - If any character is filled, the message is hidden
    
    Remembering how the Data flows throughout Templates and Components
     - 01) at Login Template, a Tag/Selector of Messages is used
     - 01.1) A property/attribute "message" is declared at the Message Tag/Selector usage, with some value.
     - 02) The value of "message" is sent to Message Component, to attribute with same name and annotated with @Input()
     - 03) The Message Component redirects to its Template, that will show the content of its variable into the HTML as it will be render info the Login Template final HTML

---
+++ catBooks/src/app/shared/messages/messages.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-messages',
+    templateUrl: './messages.component.html',
+    styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+    @Input()
+    message   =   'Mensagem de Alerta padrão!';
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
