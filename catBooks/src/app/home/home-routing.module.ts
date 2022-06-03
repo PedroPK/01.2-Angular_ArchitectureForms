762345c0efc0092e394a566cdd5ea5a150b58bd8
PedroPK <pedropk@gmail.com>
2022-06-03 17:43:22 -0300

    Added a new Component to User Registration
    No functionality yet, just the navigation throughout RouterLink

--- catBooks/src/app/home/home-routing.module.ts
+++ catBooks/src/app/home/home-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent }        from './home.component';
 import { LoginComponent }       from './login/login.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 const routes: Routes = [
     {
@@ -12,6 +13,10 @@ const routes: Routes = [
             {
                 path: '',
                 component: LoginComponent
+            },
+            {
+                path: 'newUser',
+                component:  UserRegistrationComponent
             }
         ]
     }
--- catBooks/src/app/home/home.module.ts
+++ catBooks/src/app/home/home.module.ts
@@ -6,12 +6,14 @@ import { HomeRoutingModule }  from './home-routing.module';
 import { HomeComponent }      from './home.component';
 import { LoginComponent }     from './login/login.component';
 import { MessagesModule }     from '../shared/messages/messages.module';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 
 @NgModule({
   declarations: [
       HomeComponent,
-      LoginComponent
+      LoginComponent,
+      UserRegistrationComponent
   ],
   imports: [
       CommonModule,
