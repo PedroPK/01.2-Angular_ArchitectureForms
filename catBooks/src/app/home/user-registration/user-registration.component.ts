696bb15d95a43a6173da70bea9f52818356d9f9c
PedroPK <pedropk@gmail.com>
2022-06-06 16:58:31 -0300

    Integration of FrontEnd with BackEnd to Register New User

--- catBooks/src/app/home/user-registration/user-registration.component.ts
+++ catBooks/src/app/home/user-registration/user-registration.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit }					from '@angular/core';
+import { FormBuilder, FormGroup, Validators }	from '@angular/forms';
+import { Router }								from '@angular/router';
 
-import { User }					from './user';
-import { lowercaseValidator }	from './validators/lowercase-validator';
-import { ExistingUserService } from './services/existing-user.service';
-import { userNamePasswordEqualsValidator } from './validators/username-password-equals-validator';
+import { User }								from './user';
+import { lowercaseValidator }				from './validators/lowercase-validator';
+import { ExistingUserService }				from './services/existing-user.service';
+import { userNamePasswordEqualsValidator }	from './validators/username-password-equals-validator';
+import { CreateUserService } from './services/create-user.service';
 
 @Component({
 	selector: 'app-user-registration',
@@ -17,7 +19,9 @@ export class UserRegistrationComponent implements OnInit {
 
 	constructor(
 		private formBuilder:			FormBuilder,
-		private existingUserService: 	ExistingUserService
+		private createUserService:		CreateUserService,
+		private existingUserService: 	ExistingUserService,
+		private router:					Router
 	) {}
 
 	// Will execute after Constructor and Injection of all attributes
@@ -66,8 +70,37 @@ export class UserRegistrationComponent implements OnInit {
 	}
 
 	register() {
-		const newUser		=	this.newUserForm.getRawValue() as User;
-		console.log(newUser);
+		//console.log('UserRegistrationComponent >> register() executed');
+
+		//console.log('this.newUserForm.valid = ' + this.newUserForm.valid);
+
+		if ( this.newUserForm.valid ) {
+			const newUser		=	this.newUserForm.getRawValue() as User;
+
+			//console.log(newUser);
+
+			this.createUserService.createUser(newUser).subscribe({
+				next: () => {
+					//console.log('UserRegistrationComponent: next() executed')
+					this.router.navigate(['']);
+				},
+				error: (e) => {
+					//console.log('UserRegistrationComponent: error() executed')
+					console.log(e);
+				}
+			});
+		} else {
+			// Find which field are not Valid
+			const invalid	= [];
+			const fields	=	this.newUserForm.controls;
+			for ( const field in fields ) {
+				if ( fields[field].invalid ) {
+					invalid.push(field);
+				}
+			}
+
+			//console.log('Invalid Form Fields: ' + invalid);
+		}
 	}
 
 }
