ec4c68bd832cd6b562586e3af0517956260f4f74
PedroPK <pedropk@gmail.com>
2022-06-21 21:16:41 -0300

    New Details Component
    New Routing to DetailsComponent at AnimalsRoutingModule
    
    SearchById at AnimalsService
    Refactoring to extract Methods and Reuse them
    
    At ImageGrid Template, put all ImageCard selector into <a> link selector

--- catBooks/src/app/animals/animals-routing.module.ts
+++ catBooks/src/app/animals/animals-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
 import { ListAnimalsComponent } from './list-animals/list-animals.component';
 
 const routes: Routes = [
     {
         path: '',
         component: ListAnimalsComponent
-    }
+    },
+	{
+		path:':animalId',
+		component: DetailsComponent
+	}
 ];
 
 @NgModule({
--- catBooks/src/app/animals/animals.service.ts
+++ catBooks/src/app/animals/animals.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { TokenService } from '../authentication/token.service';
-import { Animals } from './animals';
+import { Animal, Animals } from './animals';
 
 const API = environment.apiUrl;
 
@@ -17,15 +17,34 @@ export class AnimalsService {
 	) {}
 
 	listUser(pUserName: string): Observable<Animals> {
-		const token = this.tokenService.retrieveToken();
+		const token = this.getToken();
 
-		const headers = new HttpHeaders().append('x-access-token', token);
+		const headers = this.getHeaders(token);
 
 		return this.httpClient
 			.get<Animals>(
 				`${API}/${pUserName}/photos`,
 				{ headers: headers}
 			);
+	}
+
+	private getToken() {
+		return this.tokenService.retrieveToken();
+	}
+
+	private getHeaders(token: string) {
+		const X_ACCESS_TOKEN = 'x-access-token';
+		return new HttpHeaders().append(X_ACCESS_TOKEN, token);
+	}
+
+	searchById(pId: number): Observable<Animal> {
+		const token = this.getToken();
+		const headers = this.getHeaders(token);
+
+		return this.httpClient.get<Animal>(
+					`${API}/photos/${pId}`,
+					{headers}
+		);
 	}
 
 }
