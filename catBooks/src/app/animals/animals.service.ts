ab174e7b67e0c333789a70194cc872ce3c9978ed
PedroPK <pedropk@gmail.com>
2022-07-06 12:55:25 -0300

    Merging diverging Master and Origin/Master

--- catBooks/src/app/animals/animals.service.ts
+++ catBooks/src/app/animals/animals.service.ts
@@ -1,19 +1,17 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, mapTo, Observable, of, throwError } from 'rxjs';
+import { catchError, map, Observable, of, throwError } from 'rxjs';
+
 import { environment } from 'src/environments/environment';
-import { TokenService } from '../authentication/token.service';
 import { Animal, Animals } from './animals';
 
 const API = environment.apiUrl;
-const NOT_MODIFIED_HTTP_CODE	=	'304';
+const NOT_MODIFIED_HTTP_CODE = '304';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class AnimalsService {
-
-	baseUrl_ApiPhotos:	string		=	`${API}/photos/`;
 
 	constructor(
 		private httpClient:		HttpClient
@@ -28,31 +26,32 @@ export class AnimalsService {
 
 	searchById(pId: number): Observable<Animal> {
 		return this.httpClient.get<Animal>(
-					`${this.baseUrl_ApiPhotos}${pId}`
+			`${API}/photos/${pId}`
 		);
 	}
 
 	deleteAnimal(pId: number): Observable<Animal> {
 		return this.httpClient.delete<Animal>(
-			`${this.baseUrl_ApiPhotos}${pId}`
+					`${API}/photos/${pId}`
 		);
 	}
 
-	likeAnimal(pId: number): Observable<boolean> {
-		return this.httpClient.post(
-			`${this.baseUrl_ApiPhotos}${pId}/like`,
-			{},
-			{
-				observe: 'response'
-			}
-		).pipe(
-			mapTo(true),
-			catchError((error) => {
-				return error.status === NOT_MODIFIED_HTTP_CODE
-					? of(false)
-					: throwError(() => new Error(error))
-			})
-		);
+	like(pId: number) {
+		return this.httpClient
+			.post(`${API}/photos/${pId}/like`,		{},		{observe: 'response'})
+			.pipe(
+				map(() => true),
+				catchError(
+					(error) => {
+						return error.status === NOT_MODIFIED_HTTP_CODE
+							? of(false)
+							: throwError(
+								() => new Error(JSON.stringify(error))
+							)
+					}
+				)
+			)
+		;
 	}
 
 }
