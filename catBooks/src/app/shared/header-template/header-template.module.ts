fbd9448ccf0aa1825e60ad1921c2b9f77c533bee
PedroPK <pedropk@gmail.com>
2022-07-14 19:32:44 -0300

    Moving Menu folder into Shared folder

--- catBooks/src/app/shared/header-template/header-template.module.ts
+++ catBooks/src/app/shared/header-template/header-template.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { NgModule }		from '@angular/core';
+import { CommonModule }	from '@angular/common';
+import { RouterModule }	from '@angular/router';
 
-import { HeaderTemplateComponent } from './header-template.component';
-import { MenuModule } from 'src/app/components/menu/menu.module';
+import { HeaderTemplateComponent }	from './header-template.component';
+import { MenuModule }				from '../menu/menu.module';
 
 @NgModule({
 	declarations: [HeaderTemplateComponent],
